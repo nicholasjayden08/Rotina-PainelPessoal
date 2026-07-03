c541937b102570d17696bccdfcf443e847f71e69
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-07-03 18:06:11 -0300

    feat: add integrated notes module with markdown editor and autosave

---
+++ backend/src/main/java/com/nicholas/rotina/repository/NotaRepository.java
@@ -0,0 +1,10 @@
+package com.nicholas.rotina.repository;
+
+import com.nicholas.rotina.model.Nota;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface NotaRepository extends JpaRepository<Nota, Long> {
+    List<Nota> findAllByOrderByDataAtualizacaoDesc();
+}
