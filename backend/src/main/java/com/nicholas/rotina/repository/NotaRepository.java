7f6b2de42172e8387bb15bf87113f2aba017dcc6
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-08-17 15:30:27 -0300

    feat: add search and pin/unpin to notes view

--- backend/src/main/java/com/nicholas/rotina/repository/NotaRepository.java
+++ backend/src/main/java/com/nicholas/rotina/repository/NotaRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface NotaRepository extends JpaRepository<Nota, Long> {
-    List<Nota> findAllByOrderByDataAtualizacaoDesc();
+    List<Nota> findAllByOrderByFixadoDescDataAtualizacaoDesc();
 }
