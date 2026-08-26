473603ef0275317def38c2886d18ae0ba77a3c0a
nicholasjayden08 <nicholasjayden01@gmail.com>
2026-08-26 11:22:46 -0300

    feat: add weekly planning backend with draft-to-checklist flow

---
+++ backend/src/main/java/com/nicholas/rotina/repository/ItemMetaSemanalRepository.java
@@ -0,0 +1,7 @@
+package com.nicholas.rotina.repository;
+
+import com.nicholas.rotina.model.ItemMetaSemanal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemMetaSemanalRepository extends JpaRepository<ItemMetaSemanal, Long> {
+}
